337c7f881ca51569fa2587752141954965028321
pro99d <pro999999d@gmail.com>
2025-06-05 09:17:33 +0300

    updated

---
+++ lua/chadrc.lua
@@ -0,0 +1,48 @@
+-- This file needs to have same structure as nvconfig.lua 
+-- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
+-- Please read that file to know all available options :( 
+
+---@type ChadrcConfig
+local M = {}
+
+M.base46 = {
+	theme = "gruvchad",
+
+	-- hl_override = {
+	-- 	Comment = { italic = true },
+	-- 	["@comment"] = { italic = true },
+	-- },
+}
+
+
+M.nvdash = { load_on_startup = true,
+buttons = {
+{ txt = "  Find File", keys = "ff", cmd = "Telescope find_files" },
+      { txt = "  Recent Files", keys = "fo", cmd = "Telescope oldfiles" },
+      { txt = "󰈭  Find Word", keys = "fw", cmd = "Telescope live_grep" },
+      { txt = "󱥚  Themes", keys = "th", cmd = ":lua require('nvchad.themes').open()" },
+      { txt = "  Mappings", keys = "ch", cmd = "NvCheatsheet" },
+      { txt = "  Projects", keys = 'p', cmd="Telescope projects"},
+      { txt = "─", hl = "NvDashFooter", no_gap = true, rep = true },
+
+      {
+        txt = function()
+          local stats = require("lazy").stats()
+          local ms = math.floor(stats.startuptime) .. " ms"
+          return "  Loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms
+        end,
+        hl = "NvDashFooter",
+        no_gap = true,
+      },
+
+      { txt = "─", hl = "NvDashFooter", no_gap = true, rep = true },
+    },
+  }
+
+M.ui = {
+      tabufline = {
+         lazyload = false
+     }
+}
+
+return M
